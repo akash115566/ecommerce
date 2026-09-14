f5b47e979c9ddeb595119031af9c874f7d7fad29
Akash Kumar <akashkum9927@gmail.com>
2026-09-14 19:02:26 +0530

    Add React Ecommerce project

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState ,useEffect} from "react";
 import { Link } from "react-router-dom";
-import "../Styles/Navbar.css";
+import "../Styles/navbar.css";
 
 function Navbar() {
     const [offerSlide, setOfferSlide] = useState(0);
